f0d8b8504a8e8b0034a35ae6852af95c378f922c
SherMill <112295094+SherMill@users.noreply.github.com>
2024-10-25 13:14:07 -0500

    Update Exercise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.classList.add('square');
+    })
+});
