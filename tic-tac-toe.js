e6ceae7221a3dc85e3fe50208bb9da9f9c63df07
SherMill <112295094+SherMill@users.noreply.github.com>
2024-10-25 13:47:28 -0500

    Update tic-tac-toe.js Ex 3 & 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -28,3 +28,32 @@ document.querySelectorAll('.square').forEach((square, ind) => {
         }
     });
 });
+
+/*Ex 3: Sytle change when mouse hovers over a square */
+
+document.querySelectorAll('.square').forEach(square => {
+    square.addEventListener('mouseover', function(){
+        square.classList.add('hover');
+    });
+    square.addEventListener('mouseout', function(){
+        square.classList.remove('hover');
+    });
+})
+
+/*Ex 4: Checks for Winners and Does a Status Update*/
+
+function checkWinner(){
+    const winCombinations = [
+        [0, 3, 6], [3, 4, 5], [6, 7, 8],  //columns
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+        [0, 4, 8], [2, 4, 6]    //diagonals
+    ];
+    winCombinations.forEach((pWin) => {
+        if (gState[pWin[0]] && gState[pWin[0]] === gState[pWin[1]] && gState[pWin[1]] === gState[pWin[2]]){
+            document.getElementById('status').textContent = `Congratulations! ${gState[pWin[0]]} is the Winner!`;
+            document.getElementById('status').classList.add('you-won');
+        }
+    });
+}
+
+
