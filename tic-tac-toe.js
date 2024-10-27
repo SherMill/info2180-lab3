754c3a2d49ad5ce98af590b923cda87a779b86e9
SherMill <112295094+SherMill@users.noreply.github.com>
2024-10-27 17:20:16 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -17,10 +17,6 @@ function initializeGame(){
 
  /*Ex 2: X/O for clicked squares */
 
-//tracks square states and starts with player X
-let gState = Array(9), fill(null);
-let gActive = true;
-
 
 function addClickHandlers(){
     const squares = document.querySelectorAll('.square');
@@ -43,34 +39,32 @@ function addClickHandlers(){
 
 const square = document.querySelectorAll('#board > div');
 
-
-
-    squares.forEach(square => {
-        square.addEventListener('mouseover', function(){
-            square.classList.add('hover');
-        });
-        square.addEventListener('mouseout', function(){
-            square.classList.remove('hover');
-        });
+squares.forEach(square => {
+    square.addEventListener('mouseover', function(){
+        square.classList.add('hover');
     });
+    square.addEventListener('mouseout', function(){
+        square.classList.remove('hover');
+    });
+});
 
-    /*Ex 4: Checks for Winners and Does a Status Update*/
+/*Ex 4: Checks for Winners and Does a Status Update*/
 
-    function checkWinner(){
-        const winCombinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-            [0, 3, 6], [3, 4, 5], [6, 7, 8],  //columns
-            [0, 4, 8], [2, 4, 6]    //diagonals
-        ];
-        winCombinations.forEach((pWin) => {
-            if (gState[pWin[0]] && gState[pWin[0]] === gState[pWin[1]] && gState[pWin[1]] === gState[pWin[2]]){
-                document.getElementById('status').textContent = `Congratulations! ${gState[pWin[0]]} is the Winner!`;
-                document.getElementById('status').classList.add('you-won');
-                gActive = false;
-            }
-        });
-    }
-
+function checkWinner(){
+    const winCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+        [0, 3, 6], [3, 4, 5], [6, 7, 8],  //columns
+        [0, 4, 8], [2, 4, 6]    //diagonals
+    ];
+    winCombinations.forEach((pWin) => {
+        if (gState[pWin[0]] && gState[pWin[0]] === gState[pWin[1]] && gState[pWin[1]] === gState[pWin[2]]){
+            document.getElementById('status').textContent = `Congratulations! ${gState[pWin[0]]} is the Winner!`;
+            document.getElementById('status').classList.add('you-won');
+            gActive = false;
+        }
+    });
+}
+/*
     document.querySelector('.btn').addEventListener('click', () => {
         squares.forEach(square => {
             square.textContent = '';
@@ -108,4 +102,4 @@ document.getElementById(.btn:hover).addEventListener('click', function(){
     });
     document.getElementById('status').textContent = 'Move your mouse over a square and click to play an O or X.'
     document.getElementById('status').classList.remove('you-won');
-});
+});*/
