a83cf58010939df792af316d7b3ee7f3ebd43db3
SherMill <112295094+SherMill@users.noreply.github.com>
2024-10-27 22:55:03 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -55,12 +55,14 @@ function addHoverEffect(){
 /*Ex 4: Checks for Winners and Does a Status Update*/
 
 function checkWinner(){
+    const squares = document.querySelectorAll('#board > div');
     const winCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
         [0, 3, 6], [3, 4, 5], [6, 7, 8],  //columns
         [0, 4, 8], [2, 4, 6]    //diagonals
     ];
     winCombinations.forEach((pWin) => {
+        const [a, b, c] = combo
         if (gState[pWin[0]] && gState[pWin[0]] === gState[pWin[1]] && gState[pWin[1]] === gState[pWin[2]]){
             document.getElementById('status').textContent = `Congratulations! ${gState[pWin[0]]} is the Winner!`;
             document.getElementById('status').classList.add('you-won');
@@ -68,42 +70,3 @@ function checkWinner(){
         }
     });
 }
-/*
-    document.querySelector('.btn').addEventListener('click', () => {
-        squares.forEach(square => {
-            square.textContent = '';
-            square.classList.remove('X', 'O');
-        });
-        const status = document.getElementById('status');
-        status.textContent = 'Move your mouse over a square and click to play an O or X';
-        status.classList.remove('you-won');
-    });
-
-   function newGame(){
-        const squares = document.querySelectorAll('#board div');
-        squares.forEach(square => square.textContent = '');
-        const status = document.getElementById('status');
-        status.textContent = 'Move your mouse over a square and click to play an O or X';
-        status.classList.remove('you-won');
-   } 
-
-   document.querySelector('.btn').addEventListener('click', newGame);
-
-   document.querySelectorAll('#board div').forEach(square =>{ 
-        square.addEventListener('click', function(){
-            if (this.textContent === ''){
-                this.textContent = curPlayer;
-                checkWinner();
-            }
-        });
-   });
-
-document.getElementById(.btn:hover).addEventListener('click', function(){
-    initializeGame();
-    document.querySelectorAll('.square').forEach(square => {
-        square.textContent = '';
-        square.classList.remove('X', 'O');
-    });
-    document.getElementById('status').textContent = 'Move your mouse over a square and click to play an O or X.'
-    document.getElementById('status').classList.remove('you-won');
-});
